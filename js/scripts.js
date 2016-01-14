ca858d4f82c89dfe09e60b12f27f65a02a1fed2c
Abigail Rolling and Eddie Duro <e.jamesduro@gmail.com>
2016-01-14 10:44:50 -0800

    side bar button reveals sidebar nav, style updates

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,17 @@ $(document).ready(function() {
   $("h3, h4").click(function(){
     $(this).next().toggle();
   });
+
+  $("#side-button").click(function() {
+    $(".side-bar").toggle();
+    $("#side-button").toggle();
+    $("#side-button-close").show();
+  });
+
+  $("#side-button-close").click(function() {
+    $(".side-bar").toggle();
+    $("#side-button-close").toggle();
+    $("#side-button").show();
+
+  });
 });
